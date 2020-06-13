e44fae5b7ab7dfa9d8e820e30f3cc26ff2a9ad2e
zhennuowu <66862178+zhennuowu@users.noreply.github.com>
2020-06-13 14:22:51 +0800

    Add files via upload

---
+++ ProjectTemplate 2/scripts/my_script.py
@@ -0,0 +1,17 @@
+"""Script to run some part of my project."""
+
+# This adds the directory above to our Python path
+#   This is so that we can add import our custom python module code into this script
+import sys
+sys.path.append('../')
+
+# Imports
+from my_module.functions import my_func, my_other_func
+
+###
+###
+
+# PYTHON SCRIPT HERE
+
+pass
+
